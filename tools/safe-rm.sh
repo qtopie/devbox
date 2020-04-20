10824f84df0632d19f334b7b5365a3def3effdf6
artificerpi <11538418+artificerpi@users.noreply.github.com>
2020-04-20 23:05:17 +0800

    Use gnome auto clean trash function

--- tools/safe-rm.sh
+++ tools/safe-rm.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-sudo apt install -y trash-cli autotrash
+sudo apt install -y trash-cli
 sudo sh -c "echo alias rm='trash' >> /etc/profile"
 
-# Install cron job, auto empty trash for 5 days old
-#write out current crontab
-crontab -l > /tmp/auto-empty-trash
-#echo new cron into cron file
-echo "@daily /usr/bin/autotrash -d 5" >> /tmp/auto-empty-trash
-#install new cron file
-crontab /tmp/auto-empty-trash
-rm /tmp/auto-empty-trash
+# use gnome settings -> privacy -> history & trash to set autoclean
