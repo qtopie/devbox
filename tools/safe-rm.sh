b91faa9db06fe7acc6c5575b531247f730d406cb
qtopie.rw <qtopie.rw@gmail.com>
2026-08-20 23:11:26 +0800

    update cli

--- tools/safe-rm.sh
+++ tools/safe-rm.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-sudo apt install -y trash-cli
-sudo sh -c "echo alias rm='trash' >> /etc/profile"
+sudo sh -c "echo alias rm=\'gio trash\' >> /etc/profile"
 
 # use gnome settings -> privacy -> history & trash to set autoclean
